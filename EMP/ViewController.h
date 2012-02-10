d4b5c244a7786ea752d721df88f6322530d774fe
Davy Campano <davy@spicerackmedia.com>
2012-02-09 21:14:52 -0500

    Initial Commit

---
+++ EMP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EMP
+//
+//  Created by David Campano on 2/9/12.
+//  Copyright (c) 2012 Spicerack Media, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
